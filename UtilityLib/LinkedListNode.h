7cff7e978f65b45a60cbda334a842e5025860348
Karteek Mekala <Karteek.Kumar.M@gmail.com>
2018-03-31 19:15:54 -0700

    moving all template information to header file to avoid weird init hackery. Seems to be recommended pattern.

--- UtilityLib/LinkedListNode.h
+++ UtilityLib/LinkedListNode.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Globals.h"
+
 template <class T>
 class LinkedListNode
 {
@@ -18,3 +20,100 @@ public:
 	LinkedListNode<T>( T data );
 	~LinkedListNode<T>();
 };
+
+template <class T>
+void LinkedListNode<T>::addAtIndex(LinkedListNode<T> *llNodeNew, unsigned int index)
+{
+	int itCounter = 0;
+
+	LinkedListNode *llNodePrev = NULL;
+	LinkedListNode *llNodeIt = this;
+	while (itCounter != index && llNodeIt)
+	{
+		llNodePrev = llNodeIt;
+		llNodeIt = llNodeIt->m_next;
+		itCounter++;
+	}
+
+	if (llNodePrev == NULL)
+	{
+		// start of the list
+		LinkedListNode *lastNodeInNewList = llNodeNew->getLastNode();
+		lastNodeInNewList->m_next = this;
+	}
+	else if (llNodeIt == NULL)
+	{
+		// end of the list
+		llNodePrev->m_next = llNodeNew;
+	}
+	else
+	{
+		// middle of this list
+		LinkedListNode *lastNodeInNewList = llNodeNew->getLastNode();
+		llNodePrev->m_next = llNodeNew;
+		lastNodeInNewList->m_next = llNodeIt;
+	}
+}
+
+template <class T>
+inline void LinkedListNode<T>::addAtBeginning(LinkedListNode<T> * llNodeNew)
+{
+	this->addAtIndex(llNodeNew, 0);
+}
+
+template <class T>
+inline void LinkedListNode<T>::addAtEnd(LinkedListNode<T> * llNodeNew)
+{
+	this->addAtIndex(llNodeNew, MAX_UINT);
+}
+
+template <class T>
+inline LinkedListNode<T>* LinkedListNode<T>::getLastNode()
+{
+	LinkedListNode<T> *nodeIt = this;
+
+	while (nodeIt->m_next != NULL)
+		nodeIt = nodeIt->m_next;
+
+	return nodeIt;
+}
+
+template <class T>
+inline unsigned int LinkedListNode<T>::getSize()
+{
+	LinkedListNode<T> *nodeIt = this;
+
+	unsigned int count = 0;
+	while (nodeIt != NULL)
+	{
+		count++;
+		nodeIt = nodeIt->m_next;
+	}
+	return count;
+}
+
+template <class T>
+inline void LinkedListNode<T>::print()
+{
+	LinkedListNode<T> *nodeIt = this;
+
+	printf("{ ");
+	while (nodeIt != NULL)
+	{
+		printf("%d ", nodeIt->m_data);
+		nodeIt = nodeIt->m_next;
+	}
+	printf("} \n");
+}
+
+template <class T>
+LinkedListNode<T>::LinkedListNode(T data)
+{
+	this->m_data = data;
+}
+
+
+template <class T>
+LinkedListNode<T>::~LinkedListNode()
+{
+}
--- UtilityLib/Stack.h
+++ UtilityLib/Stack.h
@@ -17,3 +17,75 @@ public:
 	Stack<T>();
 	~Stack<T>();
 };
+
+template <class T>
+inline void Stack<T>::push(T data)
+{
+	LinkedListNode<T> *newTop = new LinkedListNode<T>(data);
+	if (isEmpty())
+	{
+		m_top = newTop;
+		return;
+	}
+
+	newTop->m_next = m_top;
+	m_top = newTop;
+}
+
+template <class T>
+inline T Stack<T>::pop()
+{
+	if (isEmpty())
+		return NULL;
+
+	T data = m_top->m_data;
+	m_top = m_top->m_next;
+	return data;
+}
+
+template <class T>
+inline T Stack<T>::peek()
+{
+	if (isEmpty())
+		return NULL;
+
+	return m_top->m_data;
+}
+
+template <class T>
+inline bool Stack<T>::isEmpty()
+{
+	return m_top == NULL;
+}
+
+template <class T>
+void Stack<T>::print()
+{
+	Stack<T> *reverseStack = new Stack<T>();
+
+	LinkedListNode<T> *nodeIt = m_top;
+	while (nodeIt != NULL)
+	{
+		reverseStack->push(nodeIt->m_data);
+		nodeIt = nodeIt->m_next;
+	}
+
+	printf("{ ");
+	while (!reverseStack->isEmpty())
+	{
+		T popped = reverseStack->pop();
+		printf("%d ", popped);
+	}
+	printf("} \n");
+}
+
+template <class T>
+inline Stack<T>::Stack()
+{
+	this->m_top = NULL;
+}
+
+template <class T>
+inline Stack<T>::~Stack()
+{
+}
